f736f0b54fa15966b012d967f2ef35bcb86aabd9
Yri Chomich <cpukiller1991@gmail.com>
2021-08-29 18:27:30 +0300

    Email Reminder

--- server/config/functions/cron.js
+++ server/config/functions/cron.js
@@ -9,7 +9,8 @@
  *
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
  */
-
+//import dayjs from "dayjs"
+const dayjs = require('dayjs');
 module.exports = {
   /**
    * Simple example.
@@ -18,4 +19,45 @@ module.exports = {
   // '0 1 * * 1': () => {
   //
   // }
+  '*/10 * * * * *': async (ctx) => {
+    //var reminders = await strapi.services.reminders.find();
+    var curentDate = dayjs(new Date()).format("YYYY-MM-DD");
+
+    var reminders = await strapi.query('reminders').find({"reminderDate_lte":new Date()});
+
+    //{reminderDate_gt: new Date()}
+    console.log("DATE ->",curentDate,new Date())
+    reminders.forEach( async (Reminder) => {
+      //console.log(Reminder.task_board.postanovshik)
+      if(Reminder.task_board.postanovshik && Reminder.task_board.otvetstvenni) {
+
+        var postanovshik = await strapi.query('user', 'users-permissions').findOne({id: Reminder.task_board.postanovshik});
+        //Reminder.postanovshik
+        var otvetstvenni = await strapi.query('user', 'users-permissions').findOne({id: Reminder.task_board.otvetstvenni});
+        //Reminder.otvetstvenni
+
+        var reminderDate = dayjs(Reminder.reminderDate).format("YYYY-MM-DD");
+        //console.log("DATE ->",new Date())
+        console.log("otvetstvenni---->",
+          otvetstvenni.username,
+          otvetstvenni.email)
+        console.log("🚀 ~ 5sec Reminder", reminderDate,
+          "postanovshik---->",
+          postanovshik.username,
+          postanovshik.email);
+
+        // await strapi.plugins['email'].services.email.send({
+        //   to: 'cpukiller1991@gmail.com',
+        //   from: 'joelrobuchon@strapi.io',
+        //   // cc: 'helenedarroze@strapi.io',
+        //   // bcc: 'ghislainearabian@strapi.io',
+        //   // replyTo: 'annesophiepic@strapi.io',
+        //   subject: 'Use strapi email provider successfully',
+        //   text: 'Hello world!',
+        //   html: 'Hello world!',
+        // });
+      }
+    })
+
+  },
 };
--- server/config/server.js
+++ server/config/server.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
+  cron :{
+    enabled: true
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2dee9969e43c1730149a63c356aed0e8'),
